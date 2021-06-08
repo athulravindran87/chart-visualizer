912213d88bc073d3d1ea9295811fd7d74204456b
Athul Ravindran <prasanna29387@gmail.com>
2021-06-08 23:59:52 +1000

    fixed POJO for messed up variableNames

--- excel-reader/src/main/java/com/iscout/excelreader/model/UIModel.java
+++ excel-reader/src/main/java/com/iscout/excelreader/model/UIModel.java
@@ -2,11 +2,9 @@ package com.iscout.excelreader.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Builder
-@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class UIModel {
@@ -31,4 +29,172 @@ public class UIModel {
     private String pRes;
     private String pStart;
     private String sector;
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getpBarText() {
+        return pBarText;
+    }
+
+    public void setpBarText(String pBarText) {
+        this.pBarText = pBarText;
+    }
+
+    public String getpCaption() {
+        return pCaption;
+    }
+
+    public void setpCaption(String pCaption) {
+        this.pCaption = pCaption;
+    }
+
+    public String getpClass() {
+        return pClass;
+    }
+
+    public void setpClass(String pClass) {
+        this.pClass = pClass;
+    }
+
+    public int getpComp() {
+        return pComp;
+    }
+
+    public void setpComp(int pComp) {
+        this.pComp = pComp;
+    }
+
+    public int getpCost() {
+        return pCost;
+    }
+
+    public void setpCost(int pCost) {
+        this.pCost = pCost;
+    }
+
+    public String getpDepend() {
+        return pDepend;
+    }
+
+    public void setpDepend(String pDepend) {
+        this.pDepend = pDepend;
+    }
+
+    public String getpEnd() {
+        return pEnd;
+    }
+
+    public void setpEnd(String pEnd) {
+        this.pEnd = pEnd;
+    }
+
+    public int getpGroup() {
+        return pGroup;
+    }
+
+    public void setpGroup(int pGroup) {
+        this.pGroup = pGroup;
+    }
+
+    public int getpID() {
+        return pID;
+    }
+
+    public void setpID(int pID) {
+        this.pID = pID;
+    }
+
+    public String getpLink() {
+        return pLink;
+    }
+
+    public void setpLink(String pLink) {
+        this.pLink = pLink;
+    }
+
+    public int getpMile() {
+        return pMile;
+    }
+
+    public void setpMile(int pMile) {
+        this.pMile = pMile;
+    }
+
+    public String getpName() {
+        return pName;
+    }
+
+    public void setpName(String pName) {
+        this.pName = pName;
+    }
+
+    public String getpNotes() {
+        return pNotes;
+    }
+
+    public void setpNotes(String pNotes) {
+        this.pNotes = pNotes;
+    }
+
+    public int getpOpen() {
+        return pOpen;
+    }
+
+    public void setpOpen(int pOpen) {
+        this.pOpen = pOpen;
+    }
+
+    public int getpParent() {
+        return pParent;
+    }
+
+    public void setpParent(int pParent) {
+        this.pParent = pParent;
+    }
+
+    public String getpPlanEnd() {
+        return pPlanEnd;
+    }
+
+    public void setpPlanEnd(String pPlanEnd) {
+        this.pPlanEnd = pPlanEnd;
+    }
+
+    public String getpPlanStart() {
+        return pPlanStart;
+    }
+
+    public void setpPlanStart(String pPlanStart) {
+        this.pPlanStart = pPlanStart;
+    }
+
+    public String getpRes() {
+        return pRes;
+    }
+
+    public void setpRes(String pRes) {
+        this.pRes = pRes;
+    }
+
+    public String getpStart() {
+        return pStart;
+    }
+
+    public void setpStart(String pStart) {
+        this.pStart = pStart;
+    }
+
+    public String getSector() {
+        return sector;
+    }
+
+    public void setSector(String sector) {
+        this.sector = sector;
+    }
 }
